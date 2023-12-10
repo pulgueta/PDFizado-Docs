003d509da0f6343662968cadf1abe3173c6437c5
pulgueta <retardix456@gmail.com>
2023-12-10 18:39:23 -0500

    Added sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig, squooshImageService } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://docs.pdfizado.com',
+  integrations: [tailwind(), sitemap()],
   image: {
     service: squooshImageService(),
   }
