355b1494647aae7f6abc36916dd171377fa2a6dd
pulgueta <retardix456@gmail.com>
2023-12-10 19:09:10 -0500

    Added Vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap'
+import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://docs.pdfizado.com',
   integrations: [tailwind(), sitemap()],
   image: {
-    service: squooshImageService(),
-  }
+    service: squooshImageService()
+  },
+  output: "server",
+  adapter: vercel()
 });
