58c1bbabcc63aee18c9c548f6227e0d21fe5f01d
pulgueta <retardix456@gmail.com>
2023-12-10 19:39:02 -0500

    Updated Vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://docs.pdfizado.com',
   integrations: [tailwind(), sitemap()],
   image: {
     service: squooshImageService()
   },
-  output: "server",
+  output: 'server',
   adapter: vercel()
 });
